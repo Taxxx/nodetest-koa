59326b9c56123bab912a203a37ef41361d19599c
Henrry Chuquimia <hrchuquimia@gmail.com>
2017-12-28 17:50:13 -0400

    Updated routes

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('koa-bodyparser');
 
 const indexRoutes = require('./routes/index');
 const restaurantRoutes = require('./routes/restaurants');
+const reviewRoutes = require('./routes/reviews');
+const mealRoutes = require('./routes/meals');
+const orderRoutes = require('./routes/orders');
 
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
@@ -10,6 +13,9 @@ const PORT = process.env.PORT || 1337;
 app.use(bodyParser());
 app.use(indexRoutes.routes());
 app.use(restaurantRoutes.routes());
+app.use(reviewRoutes.routes());
+app.use(mealRoutes.routes());
+app.use(orderRoutes.routes());
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
